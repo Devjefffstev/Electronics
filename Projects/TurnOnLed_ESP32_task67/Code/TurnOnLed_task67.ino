f6c7937cc564e51669708de6a2f479833c203f07
David Farias <davidndfv10@gmail.com>
2025-12-29 11:56:46 -0500

    Keep only TurnOnLed_task67.ino directly inside Code folder and remove duplicate subfolder

--- Projects/TurnOnLed_ESP32_task67/Code/TurnOnLed_task67.ino
+++ Projects/TurnOnLed_ESP32_task67/Code/TurnOnLed_task67.ino
@@ -1,10 +1,11 @@
 void setup() {
-  pinMode(4, OUTPUT); // declare GPIO4 pin as output
+  pinMode(4, OUTPUT); // declaramos el pin GPIO19 como salida
 }
 
 void loop() {
-  digitalWrite(4, HIGH); // turn LED on
-  delay(1000);           // wait 1 second
-  digitalWrite(4, LOW);  // turn LED off
-  delay(1000);           // wait 1 second
+  digitalWrite(4, HIGH); // encendido del LED
+  delay(1000);            // espera 1 segundo
+  digitalWrite(4, LOW);  // apagado del LED
+  delay(1000);            // espera 1 segundo
 }
+
